f8fafd3268e9e533daf014d72784c6181c90cb0c
Tahim Bhuiya <tbhuiya2018@fau.edu>
2025-12-16 17:25:53 -0500

    Added comments

--- bellman_ford.cpp
+++ bellman_ford.cpp
@@ -18,7 +18,7 @@ void bellmanFord(int V, int src, vector<Edge>& edges) {
     // Distance array: dist[i] will hold the shortest distance from src to i
     vector<int> dist(V, INT_MAX);
 
-   
+    // Distance to source is 0
     
 
     
