d5d0b63fc594db9bfddd047131885bfe8e6c5497
Tahim Bhuiya <tbhuiya2018@fau.edu>
2025-12-19 18:10:18 -0500

    Added onto bellmanFord function

--- bellman_ford.cpp
+++ bellman_ford.cpp
@@ -50,7 +50,7 @@ void bellmanFord(int V, int src, vector<Edge>& edges) {
     // Print shortest distances
     cout << "Shortest distances from source node " << src << ":\n";
     for (int i = 0; i < V; i++) {
-        
+        if (dist[i] == INT_MAX)
             
         
             
